d52d59bc312ffbacb64b4c4c64a86100e0d7c7cf
sebgos <gos.sebastian@gmail.com>
2020-04-24 12:56:17 +0200

    feat(feature-a): add feature-a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeatureAComponent } from './feature-a/feature-a.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FeatureAComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
